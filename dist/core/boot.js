d7ea4b1e9390ebd320894088a2cabbe3e47844b7
Jeff Schwartz <jefftschwartz@gmail.com>
2016-04-28 18:40:10 -0400

    Added boot script to boot preamble core.

---
+++ dist/core/boot.js
@@ -0,0 +1,8 @@
+/**
+ * Run preamble-ts-core
+ * The call to require below returns premalbe-ts-core's dist/src/main.js.
+ * See the browserify task in preamble-ts-core's gulpfile.js, which generates the bundle.
+ */
+"use strict";
+var boot = require("main");
+boot();
