6179d79b4858f7b050f5181250936177d1c0dc41
Jeff Schwartz <jefftschwartz@gmail.com>
2016-04-28 18:38:19 -0400

    Simplified global setup.

--- dist/config/preambleConfig.js
+++ dist/config/preambleConfig.js
@@ -1,22 +1,16 @@
 /**
- *Use this file to override any of Preamble"s default configuration settings.
- *Don"t forget to uncomment this file"s script tag in SpecRunner.html.
+ * Use this file to override Preamble"s default configuration settings.
+ * Don"t forget to uncomment this file"s script tag in SpecRunner.html.
  */
 
 "use strict";
 
-var preambleGlobal;
-if (typeof(window) !== "undefined") {
-    preambleGlobal = window;
-} else if (typeof(global) !== "undefined") {
-    preambleGlobal = global;
-}
-
-preambleGlobal.preambleConfig = {
-    windowGlobals: true,
+var preambleGlobal = window;
+preambleGlobal.preamble = preambleGlobal.preamble || {};
+preambleGlobal.preamble.preambleConfig = {
     timeoutInterval: 5000,
     name: "Sanity Check",
     uiTestContainerId: "ui-test-container",
     hidePassedTests: false,
-    shortCircuit: false
+    shortCircuit: true
 };
