41011602b8aaec744a7f080ef5ba72842ab368b3
Jeff Schwartz <jefftschwartz@gmail.com>
2016-05-04 17:28:49 -0400

    Added accidentally deleted dist/core/ folders. No longer using subfolders in dist/.

---
+++ dist/config/preambleConfig.js
@@ -0,0 +1,16 @@
+/**
+ * Use this file to override Preamble"s default configuration settings.
+ * Don"t forget to uncomment this file"s script tag in SpecRunner.html.
+ */
+
+"use strict";
+
+var preambleGlobal = window;
+preambleGlobal.preamble = preambleGlobal.preamble || {};
+preambleGlobal.preamble.preambleConfig = {
+    timeoutInterval: 5000,
+    name: "Sanity Check",
+    uiTestContainerId: "ui-test-container",
+    hidePassedTests: false,
+    shortCircuit: false
+};
---
+++ dist/core/preamble-matchers.js
@@ -0,0 +1,162 @@
+(function () {
+    var registerMatchers = [];
+    var register = function (registerMatcher, comparators) {
+        registerMatcher({
+            apiName: "toBeTrue",
+            api: function () { },
+            evaluator: function (expectedValue) { return expectedValue === true; },
+            negator: true,
+            minArgs: 0,
+            maxArgs: 0
+        });
+        registerMatcher({
+            apiName: "toBeTruthy",
+            api: function () { },
+            evaluator: function (expectedValue) { return !!expectedValue; },
+            negator: true,
+            minArgs: 0,
+            maxArgs: 0
+        });
+        registerMatcher({
+            apiName: "toBe",
+            api: function (matcherValue) { return matcherValue; },
+            evaluator: function (expectedValue, matcherValue) {
+                return expectedValue === matcherValue;
+            },
+            negator: true,
+            minArgs: 1,
+            maxArgs: 1
+        });
+        registerMatcher({
+            apiName: "toEqual",
+            api: function (matcherValue) { return matcherValue; },
+            evaluator: function (expectedValue, matcherValue) {
+                return comparators.deepRecursiveCompare(expectedValue, matcherValue);
+            },
+            negator: true,
+            minArgs: 1,
+            maxArgs: 1
+        });
+        registerMatcher({
+            apiName: "toBeDefined",
+            api: function () { },
+            evaluator: function (expectedValue) { return expectedValue !== undefined; },
+            negator: true,
+            minArgs: 0,
+            maxArgs: 0
+        });
+        registerMatcher({
+            apiName: "toBeUndefined",
+            api: function () { },
+            evaluator: function (expectedValue) { return expectedValue === undefined; },
+            negator: true,
+            minArgs: 0,
+            maxArgs: 0
+        });
+        registerMatcher({
+            apiName: "toBeNull",
+            api: function () { },
+            evaluator: function (expectedValue) { return expectedValue === null; },
+            negator: true,
+            minArgs: 0,
+            maxArgs: 0
+        });
+        registerMatcher({
+            apiName: "toMatch",
+            api: function (matcherValue) { return matcherValue; },
+            evaluator: function (expectedValue, matcherValue) {
+                return matcherValue.exec(expectedValue) !== null;
+            },
+            negator: true,
+            minArgs: 1,
+            maxArgs: 1
+        });
+        registerMatcher({
+            apiName: "toHaveBeenCalled",
+            api: function () { },
+            evaluator: function (expectedValue) {
+                return expectedValue.calls.count() > 0;
+            },
+            negator: true,
+            minArgs: 0,
+            maxArgs: 0
+        });
+        registerMatcher({
+            apiName: "toHaveBeenCalledWith",
+            api: function () {
+                var matcherValue = [];
+                for (var _i = 0; _i < arguments.length; _i++) {
+                    matcherValue[_i - 0] = arguments[_i];
+                }
+                return matcherValue;
+            },
+            evaluator: function (expectedValue, matcherValue) {
+                return expectedValue.calls.wasCalledWith.apply(null, matcherValue);
+            },
+            negator: true,
+            minArgs: 1,
+            maxArgs: -1
+        });
+        registerMatcher({
+            apiName: "toHaveBeenCalledWithContext",
+            api: function (matcherValue) { return matcherValue; },
+            evaluator: function (expectedValue, matcherValue) {
+                return expectedValue.calls.wasCalledWithContext(matcherValue);
+            },
+            negator: true,
+            minArgs: 1,
+            maxArgs: 1
+        });
+        registerMatcher({
+            apiName: "toHaveReturnedValue",
+            api: function (matcherValue) { return matcherValue; },
+            evaluator: function (expectedValue, matcherValue) {
+                return expectedValue.calls.returned(matcherValue);
+            },
+            negator: true,
+            minArgs: 1,
+            maxArgs: 1
+        });
+        registerMatcher({
+            apiName: "toHaveThrown",
+            api: function () { },
+            evaluator: function (expectedValue) {
+                return expectedValue.calls.threw();
+            },
+            negator: true,
+            minArgs: 0,
+            maxArgs: 0
+        });
+        registerMatcher({
+            apiName: "toHaveThrownWithMessage",
+            api: function (matcherValue) { return matcherValue; },
+            evaluator: function (expectedValue, matcherValue) {
+                return expectedValue.calls.threwWithMessage(matcherValue);
+            },
+            negator: true,
+            minArgs: 1,
+            maxArgs: 1
+        });
+        registerMatcher({
+            apiName: "toHaveThrownWithName",
+            api: function (matcherValue) { return matcherValue; },
+            evaluator: function (expectedValue, matcherValue) {
+                return expectedValue.calls.threwWithName(matcherValue);
+            },
+            negator: true,
+            minArgs: 1,
+            maxArgs: 1
+        });
+    };
+    var preambleGlobal;
+    preambleGlobal = typeof window !== "undefined" ? window : global;
+    var pGlobal = preambleGlobal;
+    if (!pGlobal.hasOwnProperty("preamble")) {
+        pGlobal.preamble = { registerMatchers: registerMatchers };
+    }
+    else {
+        pGlobal.preamble.registerMatchers = registerMatchers;
+    }
+    registerMatchers.push(register);
+}());
+//# sourceMappingURL=preamble-matchers.js.map
